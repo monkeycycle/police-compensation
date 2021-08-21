b4af836e9af00f3f61945fecf562534792cdc15b
Michael Pereira <monkeycycle@gmail.com>
2021-08-20 19:26:31 -0500

    Adding folos

--- R/__wps_wfps_salary_100up.R
+++ R/__wps_wfps_salary_100up.R
@@ -12,6 +12,17 @@ wps_wfps_salary_disclosure_all <- df_winnipeg_salary_disclosure %>%
   )
 
 
+wps_wfps_salary_disclosure_all_less100 <- df_winnipeg_salary_disclosure %>%
+  filter(compensation < 100000) %>%
+  filter(
+    department %in% c("Police Services", "Fire Paramedic Service")
+  ) %>%
+  group_by(department, year) %>%
+  tally() %>%
+  ungroup() %>%
+  rename(
+    subset_less100 = n
+  )
 
 wps_wfps_salary_disclosure_all_100_150 <- df_winnipeg_salary_disclosure %>%
   filter(compensation >= 100000 & compensation <= 150000 ) %>%
@@ -69,26 +80,39 @@ wps_wfps_salary_disclosure_all_200plus <- df_winnipeg_salary_disclosure %>%
 
 
 wps_wfps_sixfigure <- left_join(
-  wps_wfps_salary_disclosure_all,
   wps_wfps_salary_disclosure_all_100_150,
-  by=c("department"="department", "year"="year")
-)
-
-wps_wfps_sixfigure <- left_join(
-  wps_wfps_sixfigure,
   wps_wfps_salary_disclosure_all_150_200,
   by=c("department"="department", "year"="year")
-)
-
-wps_wfps_sixfigure <- left_join(
-  wps_wfps_sixfigure,
+) %>%
+left_join(
   wps_wfps_salary_disclosure_all_200plus,
   by=c("department"="department", "year"="year")
-)
+) %>%left_join(
+  wps_wfps_salary_disclosure_all_less100,
+  by=c("department"="department", "year"="year")
+) %>%
+  pivot_longer(
+    c(-year, -department),
+    names_to="salary_group",
+    values_to="count"
+  ) %>%
+  mutate(
+    salary_group = gsub("subset_", "$", salary_group, fixed=TRUE),
+    salary_group = gsub("_", "-", salary_group, fixed=TRUE),
+    salary_group = gsub("200plus", "200K or more", salary_group, fixed=TRUE),
+    salary_group = gsub("$less100", "Less than $100K", salary_group, fixed=TRUE)
+  ) %>%
+  mutate(
+    salary_group = factor(salary_group,
+                          levels=c("$200K or more", "$150-200K", "$100-150K", "Less than $100K")
+    )
+  )
 
 
 p_wps_wfps_6figure_salary <- ggplot(wps_wfps_sixfigure) +
-  geom_bar(data=wps_wfps_salary_disclosure_all, stat="identity",aes(x=year, y=all_disclosed), fill="#999999") +
+  geom_bar(data=wps_wfps_sixfigure, stat="identity",aes(x=year, y=count, fill=salary_group), position="stack") +
+  geom_text(data=wps_wfps_salary_disclosure_all, stat="identity",aes(x=year, y=all_disclosed, label=format(all_disclosed, digit=0, big.mark=",")), size=3.5, vjust=-.5) +
+
   labs(
     title = wrap_text("Number of WPS and WFPS staff with salaries above $100,000", 65),
     colour="",
@@ -99,6 +123,15 @@ p_wps_wfps_6figure_salary <- ggplot(wps_wfps_sixfigure) +
       "WINNIPEG FREE PRESS — SOURCE: City of Winnipeg"
     ))
   ) +
+  # scale_x_date(expand = c(0, 0),
+  #              date_breaks = "1 year", labels = date_format("%Y"),
+  #              limits=as.Date(c("2016-01-01", "2020-01-01"))) +
+  scale_y_continuous(
+    expand = c(0, 0),
+    limits = c(0, 2000),
+    labels = scales::comma
+  ) +
+  scale_fill_manual(values=wfp_colour_scale_muted) +
   minimal_theme() +
   theme(
     strip.background=ggplot2::element_blank(),
@@ -111,5 +144,7 @@ p_wps_wfps_6figure_salary <- ggplot(wps_wfps_sixfigure) +
   ) +
   facet_wrap(.~department, ncol=2)
 
-plot(p_wps_wfps_6figure_salary)
+
+wfp_wps_wfps_6figure_salary <- prepare_plot(p_wps_wfps_6figure_salary)
+ggsave_pngpdf(wfp_wps_wfps_6figure_salary, "wfp_wps_wfps_6figure_salary", width_var=8.66, height_var=6, dpi_var=300, scale_var=1, units_var="in")
 
--- R/visualize.R
+++ R/visualize.R
@@ -1,5 +1,5 @@
-wfp_colour_scale_bold <- c(wfp_blue, nominalBold_shade_1, nominalBold_shade_2, nominalBold_shade_3, nominalBold_shade_4)
-wfp_colour_scale_muted <- c(wfp_blue, nominalMuted_shade_1, nominalMuted_shade_2, nominalMuted_shade_3, nominalMuted_shade_4)
+wfp_colour_scale_bold <- c(nominalBold_shade_0, nominalBold_shade_1, nominalBold_shade_2, nominalBold_shade_3, nominalBold_shade_4)
+wfp_colour_scale_muted <- c(nominalMuted_shade_0, nominalMuted_shade_1, nominalMuted_shade_2, nominalMuted_shade_3, nominalMuted_shade_4)
 
 source(dir_src("visualize_wps_wfps_budgets.R"))
 source(dir_src("visualize_wps_annual_report_costs.R"))
